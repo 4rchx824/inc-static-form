c245a2e15dc8e16789817328cb89a3fa1e535793
4rchx824 <archielaw824@gmail.com>
2023-11-18 13:32:46 +0800

    git auth working

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,8 +4,9 @@ import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
+  Awaitable,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
@@ -48,9 +49,9 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
